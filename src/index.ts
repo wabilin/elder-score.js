34ee623dcaba0db553de4f604b18cf58d143b022
wabilin <twelendil@protonmail.com>
2019-03-11 10:16:38 +0800

    config test and tried two functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 const _ = {
-  concat<T>(head: T[], ...tail: T[][]) {
-    return head.concat(...tail);
-  }
+  concat<T>(...arrays: (T[]|T)[]): T[] {
+    const ary: T[] = [];
+    return ary.concat(...arrays)
+  },
+
+  fill<T>(array: T[], value: T, start = 0, end=array.length): T[] {
+    return array.fill(value, start, end);
+  },
 };
 
 export default _;
