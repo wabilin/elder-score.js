5d06be68915830dbd6a88157748981fc3b07f98b
wabilin <twelendil@protonmail.com>
2019-08-30 23:50:29 +0900

    yarn-upgrade-190831

--- dist/_.d.ts
+++ dist/_.d.ts
@@ -1,28 +1,34 @@
 interface Dict<V> {
     [key: string]: V;
 }
+interface FuncObj {
+    [key: string]: Function | unknown;
+}
 declare const _: {
     head: <T>(array: T[], n?: number | undefined) => T | T[];
-    tail: <T>(array: T[], index?: number) => T[];
-    drop: <T>(array: T[], index?: number) => T[];
-    dropRight: <T>(array: T[], n?: number) => T[];
+    tail: <T_1>(array: T_1[], index?: number) => T_1[];
+    drop: <T_1>(array: T_1[], index?: number) => T_1[];
+    dropRight: <T_2>(array: T_2[], n?: number) => T_2[];
     assign: {
-        <T, U>(target: T, source: U): T & U;
-        <T, U, V>(target: T, source1: U, source2: V): T & U & V;
-        <T, U, V, W>(target: T, source1: U, source2: V, source3: W): T & U & V & W;
+        <T_3, U>(target: T_3, source: U): T_3 & U;
+        <T_4, U_1, V>(target: T_4, source1: U_1, source2: V): T_4 & U_1 & V;
+        <T_5, U_2, V_1, W>(target: T_5, source1: U_2, source2: V_1, source3: W): T_5 & U_2 & V_1 & W;
         (target: object, ...sources: any[]): any;
     };
     entries: {
-        <T>(o: {
-            [s: string]: T;
-        } | ArrayLike<T>): [string, T][];
+        <T_6>(o: {
+            [s: string]: T_6;
+        } | ArrayLike<T_6>): [string, T_6][];
         (o: {}): [string, any][];
     };
-    keys: (o: {}) => string[];
+    keys: {
+        (o: object): string[];
+        (o: {}): string[];
+    };
     values: {
-        <T>(o: {
-            [s: string]: T;
-        } | ArrayLike<T>): T[];
+        <T_7>(o: {
+            [s: string]: T_7;
+        } | ArrayLike<T_7>): T_7[];
         (o: {}): any[];
     };
     isInteger: (number: number) => boolean;
@@ -32,24 +38,34 @@ declare const _: {
     isArray: (arg: any) => arg is any[];
     isArguments(x: unknown): boolean;
     isBoolean(x: unknown): x is boolean;
-    isEmpty<T>(x: object | T[] | null | undefined): boolean;
+    isEmpty<T_8>(x: object | T_8[] | null | undefined): boolean;
+    isObject(x: unknown): x is object;
     isObjectLike(x: unknown): x is object;
-    compact<T>(elements: T[]): T[];
-    difference<T>(a: T[], b: T[]): T[];
-    fromEntries<T>(pairs: [string, T][]): Dict<T>;
-    flatten<T = any>(array: any[], depth?: number): T[];
+    range(start: number, end?: number | undefined, step?: number | undefined): number[];
+    chunk<T_9>(array: T_9[], size?: number): T_9[][];
+    compact<T_10>(elements: T_10[]): T_10[];
+    difference<T_11>(a: T_11[], b: T_11[]): T_11[];
+    fromEntries<T_12>(pairs: [string, T_12][]): Dict<T_12>;
+    flatten<T_13 = any>(array: any[], depth?: number): T_13[];
     first<T>(array: T[], n?: number | undefined): T | T[];
-    initial<T>(array: T[], n?: number): T[];
-    intersection<T>(...arrays: T[][]): T[];
-    last<T>(array: T[], n?: number | undefined): T | T[] | undefined;
-    rest<T>(array: T[], index?: number): T[];
-    take<T>(array: T[], n?: number): T[];
-    unzip<T>(arrays: T[][]): T[][];
-    without<T>(array: T[], ...values: T[]): T[];
-    union<T>(array: T[], ...rest: T[][]): T[];
-    uniq<T>(array: T[]): T[];
-    zip<T>(...arrays: T[][]): (T | undefined)[][];
+    initial<T_2>(array: T_2[], n?: number): T_2[];
+    findLastIndex<T_14>(array: T_14[], predicate: (ele: T_14) => boolean): number;
+    intersection<T_15>(...arrays: T_15[][]): T_15[];
+    last<T_16>(array: T_16[], n?: number | undefined): T_16 | T_16[] | undefined;
+    rest<T_1>(array: T_1[], index?: number): T_1[];
+    take<T_17>(array: T_17[], n?: number): T_17[];
+    sortedIndex<T_18>(array: T_18[], val: T_18): number;
+    unzip<T_19>(arrays: T_19[][]): T_19[][];
+    without<T_20>(array: T_20[], ...values: T_20[]): T_20[];
+    union<T_21>(array: T_21[], ...rest: T_21[][]): T_21[];
+    uniq<T_22>(array: T_22[]): T_22[];
+    zip<T_23>(...arrays: T_23[][]): (T_23 | undefined)[][];
     max(array: number[]): number;
+    has(obj: object, key: string | number | symbol): boolean;
+    bindAll<T_24 extends FuncObj>(object: T_24, ...methodNames: (keyof T_24)[]): void;
+    memoize<K extends string | number, T_25, R, TH>(fun: (key: K, ...rest: T_25[]) => R, hasher?: Function | undefined): (this: TH, key: K, ...rest: T_25[]) => {
+        [key: string]: R;
+    }[K];
 };
 export default _;
 //# sourceMappingURL=_.d.ts.map
--- dist/_.js
+++ dist/_.js
@@ -8,16 +8,34 @@ const flatDepth = (array, depth) => {
         return acc.concat(val);
     }, []);
 };
+const endIndex = (array) => array.length - 1;
+const bSearch = (array, val, low, high) => {
+    if (low >= high) {
+        return low;
+    }
+    const m = Math.floor((low + high) / 2);
+    if (array[m] < val) {
+        return bSearch(array, val, m + 1, high);
+    }
+    return bSearch(array, val, low, m);
+};
+const range = (start, end, step = 1) => {
+    const len = Math.max(Math.ceil((end - start) / step), 0);
+    // tslint:disable-next-line: prefer-array-literal
+    const ary = Array(len);
+    for (let val = start, i = 0; i < len; i += 1, val += step) {
+        ary[i] = val;
+    }
+    return ary;
+};
 const { assign, entries, keys, values } = Object;
 const { isInteger, isNaN, isFinite, isSafeInteger } = Number;
 const { isArray } = Array;
 // e for Elder Score
 const e = {
     // functions from Object
-    // TODO: Add to Spec
     assign, entries, keys, values,
     // functions from Number
-    // TODO: Add to Spec
     isInteger, isNaN, isFinite, isSafeInteger,
     // functions from Array
     isArray,
@@ -36,8 +54,23 @@ const e = {
         }
         return _.isEmpty(keys(x));
     },
+    isObject(x) {
+        const t = typeof x;
+        return (t === 'object' && !!x) || t === 'function';
+    },
     isObjectLike(x) {
         return typeof x === 'object' && x !== null;
+    },
+    range(start, end, step) {
+        if (end === undefined) {
+            return range(0, start, step);
+        }
+        return range(start, end, step);
+    },
+    chunk(array, size = 1) {
+        const { length } = array;
+        return range(0, Math.ceil(length / size) * size, size)
+            .map(i => array.slice(i, i + size));
     },
     compact(elements) {
         return elements.filter(Boolean);
@@ -51,7 +84,7 @@ const e = {
         pairs.forEach(([key, val]) => { obj[key] = val; });
         return obj;
     },
-    // TODO: use Array.prototype.flat when the standard release.
+    // NOTE: use Array.prototype.flat when the standard release.
     flatten(array, depth = 1) {
         return flatDepth(array, depth);
     },
@@ -59,7 +92,15 @@ const e = {
         return n ? array.slice(0, n) : array[0];
     },
     initial(array, n = 1) {
-        return _.first(array, array.length - n);
+        return (_.first(array, array.length - n));
+    },
+    findLastIndex(array, predicate) {
+        for (let i = endIndex(array); i >= 0; i -= 1) {
+            if (predicate(array[i])) {
+                return i;
+            }
+        }
+        return -1;
     },
     intersection(...arrays) {
         if (_.isEmpty(arrays)) {
@@ -78,6 +119,9 @@ const e = {
     },
     take(array, n = 1) {
         return array.slice(0, n);
+    },
+    sortedIndex(array, val) {
+        return bSearch(array, val, 0, array.length);
     },
     unzip(arrays) {
         if (_.isEmpty(arrays)) {
@@ -105,8 +149,26 @@ const e = {
     max(array) {
         return Math.max(...array);
     },
+    has(obj, key) {
+        return obj.hasOwnProperty(key);
+    },
+    bindAll(object, ...methodNames) {
+        methodNames.forEach(name => {
+            const old = object[name];
+            object[name] = old.bind(object);
+        });
+    },
+    memoize(fun, hasher) {
+        const cache = {};
+        return function (key, ...rest) {
+            if (!e.has(cache, key)) {
+                cache[key] = fun.apply(this, [key, ...rest]);
+            }
+            return cache[key];
+        };
+    },
 };
 // alias
-const _ = Object.assign({}, e, { head: e.first, tail: e.rest, drop: e.rest, dropRight: e.initial });
+const _ = Object.assign(Object.assign({}, e), { head: e.first, tail: e.rest, drop: e.rest, dropRight: e.initial });
 exports.default = _;
 //# sourceMappingURL=_.js.map
